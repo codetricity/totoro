f88ea44ef77d59a723fa666e15c381aa3a9065e3
Caitlyn <craigoda+caitlynbakery@gmail.com>
2017-10-18 07:26:35 -0700

    add bush

--- main3.py
+++ main3.py
@@ -1,4 +1,8 @@
 import pygame
+pygame.init()
+
+clock = pygame.time.Clock()
+FPS = 30
 
 screenwidth = 1280
 screenheight = 720
@@ -7,12 +11,50 @@ screen = pygame.display.set_mode((screenwidth, screenheight))
 
 forest = pygame.image.load("img/forestoil.jpg")
 totororight = pygame.image.load("img/totororight.png")
+totoroleft = pygame.image.load("img/totoroleft.png")
+# tree = pygame.image.load("img/elmtree.png")
+bush = pygame.image.load("img/bush.png")
+
+totoro = totororight
+
+totorosong = pygame.mixer.Sound("snd/totoro-song.wav")
+totorosong.play()
+
+totorox = 291
+totoroy = 402
+
+# treex = 1000
+bushx = 1000
+backgroundx = 0
+
+direction = "right"
 gameon = True
 
 while gameon:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameon = False
-    screen.blit(forest, (0, 0))
-    screen.blit(totororight, (291, 402))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = "right"
+                totoro = totororight
+            if event.key == pygame.K_LEFT:
+                direction = "left"
+                totoro = totoroleft
+    screen.blit(forest, (backgroundx, 0))
+    # screen.blit(tree, (treex, 250))
+    screen.blit(bush, (bushx, 520))
+    screen.blit(totoro, (totorox, totoroy))
+    
+
+    if direction == "right":
+        backgroundx = backgroundx - 1
+        # treex = treex - 3
+        bushx = bushx - 3
+    if direction == "left":
+        backgroundx = backgroundx + 1
+        # treex = treex + 3
+        bushx = bushx - 3
+    
+    clock.tick(FPS)
     pygame.display.update()
