47957f77281bb2833c2e1b9bf407be2962cba3bf
Caitlyn <craigoda+caitlynbakery@gmail.com>
2017-10-21 08:58:34 -0700

    made totoro jump

--- main3.py
+++ main3.py
@@ -13,11 +13,18 @@ forest = pygame.image.load("img/forestoil.jpg")
 totororight = pygame.image.load("img/totororight.png")
 totoroleft = pygame.image.load("img/totoroleft.png")
 
+whitetotoro = pygame.image.load("img/whitetotorosmall.png")
+whitetotoroRect = whitetotoro.get_rect(x=100, y=620)
+
 totororect = totororight.get_rect()
 # tree = pygame.image.load("img/elmtree.png")
 bush = pygame.image.load("img/bush.png")
 bushrect = bush.get_rect(x=1000, y=520)
-bush2rect = bush.get_rect(x=2300, y=520)
+bush2rect = bush.get_rect(x=2700, y=520)
+bushCollisionRect = pygame.Rect(1000, 520, 120, 120)
+bushCollisionRect.bottom = bushrect.bottom
+bushCollisionRect2 = pygame.Rect(2700, 520, 120, 120)
+bushCollisionRect2.bottom = bush2rect.bottom
 
 totoro = totororight
 
@@ -33,6 +40,7 @@ backgroundx = 0
 
 speed = 1
 direction = "right"
+jumpdirection = "stop"
 gameon = True
 
 while gameon:
@@ -46,10 +54,14 @@ while gameon:
             if event.key == pygame.K_LEFT:
                 direction = "left"
                 totoro = totoroleft
+            if event.key == pygame.K_SPACE:
+                if jumpdirection == "stop":
+                    jumpdirection = "up"
     
     screen.blit(forest, (backgroundx, 0))
     # screen.blit(tree, (treex, 250))
     screen.blit(totoro, totororect)
+    screen.blit(whitetotoro, whitetotoroRect)
     screen.blit(bush, bushrect)
     screen.blit(bush, (bush2rect))
     
@@ -58,17 +70,33 @@ while gameon:
         if backgroundx >= -4147 + 1280:
             backgroundx = backgroundx - speed
             bushrect.centerx = bushrect.centerx - speed - 2
+            bushCollisionRect.centerx = bushrect.centerx
             bush2rect.centerx = bush2rect.centerx - speed - 2
+            bushCollisionRect2.centerx = bush2rect.centerx
     if direction == "left":
         if not backgroundx >= 1:
             backgroundx = backgroundx + speed
             # treex = treex + 3
             bushrect.centerx = bushrect.centerx + speed + 2
+            bushCollisionRect.centerx = bushrect.centerx
             bush2rect.centerx = bush2rect.centerx - speed - 2
+            bushCollisionRect2.centerx = bush2rect.centerx
+    if jumpdirection == "up":
+        if totororect.top > 15:
+            totororect.centery = totororect.centery - 5
+            whitetotoroRect.centery = whitetotoroRect.centery - 5
+        else:
+            jumpdirection = "down"
+    if jumpdirection == "down":
+        if totororect.centery < 570:
+            totororect.centery = totororect.centery + 3
+            whitetotoroRect.centery = whitetotoroRect.centery + 3
+        else:
+            jumpdirection = "stop"
     
-    if totororect.colliderect(bushrect):
+    if totororect.colliderect(bushCollisionRect):
         print("murp")
-    if totororect.colliderect(bush2rect):
+    if totororect.colliderect(bushCollisionRect2):
         print("meh")
     clock.tick(FPS)
     pygame.display.update()
